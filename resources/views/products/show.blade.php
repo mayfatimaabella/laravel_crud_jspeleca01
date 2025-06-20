cd86f60a8d3a8289c0a4b4d65be1248cae1381b6
mayfatimaabella <mayfatimabella.com>
2025-06-20 03:14:14 -0600

    show image

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -35,20 +35,31 @@
                 </div>
                 
                 <div class="row">
-    <label for="price" class="col-md-4 col-form-label text-md-end text-start"><strong>Price:</strong></label>
-    <div class="col-md-6" style="line-height:35px;">
-        {{ $product->price }}
-    </div>
-</div>
+                    <label for="price" class="col-md-4 col-form-label text-md-end text-start"><strong>Price:</strong></label>
+                    <div class="col-md-6" style="line-height:35px;">
+                        {{ $product->price }}
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <label for="description" class="col-md-4 col-form-label text-md-end text-start"><strong>Description:</strong></label>
+                    <div class="col-md-6" style="line-height:35px;">
+                        {{ $product->description }}
+                    </div>
+                </div>
 
-<div class="row">
-    <label for="description" class="col-md-4 col-form-label text-md-end text-start"><strong>Description:</strong></label>
-    <div class="col-md-6" style="line-height:35px;">
-        {{ $product->description }}
-    </div>
-</div>
-</div>
-</div>
-</div> 
+                <div class="row">
+                    <label class="col-md-4 col-form-label text-md-end"><strong>Image:</strong></label>
+                    <div class="col-md-6" style="line-height: 35px;">
+                        @if($product->image_blob)
+                        <img src="data:image/jpeg;base64,{{ base64_encode($product->image_blob) }}" width="150">
+                        @else
+                        No Image
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> 
 </div> 
 @endsection 
