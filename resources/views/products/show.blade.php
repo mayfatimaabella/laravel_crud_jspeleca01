12eb32f161ba49836c89b0c497a5320ea9632e23
mayfatimaabella <mayfatimabella@gmail.com>
2025-06-24 09:36:47 +0800

    ye

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @section('content')
+
 <div class="row justify-content-center mt-3">
     <div class="col-md-8">
         <div class="card">
@@ -12,77 +13,53 @@
                 </div>
             </div>
             <div class="card-body">
-
                 <div class="row">
                     <label for="code" class="col-md-4 col-form-label text-md-end text-start"><strong>Code:</strong></label>
-                    <div class="col-md-6" style="line-height: 35px;">
+                    <div class="col-md-6" style="line-height:35px;"> 
                         {{ $product->code }}
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <label for="name" class="col-md-4 col-form-label text-md-end text-start"><strong>Name:</strong></label>
+                    <div class="col-md-6" style="line-height:35px;">
+                        {{ $product->name }}
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <label for="quantity" class="col-md-4 col-form-label text-md-end text-start"><strong>Quantity:</strong></label>
+                    <div class="col-md-6" style="line-height:35px;">
+                        {{ $product->quantity }}
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <label for="price" class="col-md-4 col-form-label text-md-end text-start"><strong>Price:</strong></label>
+                    <div class="col-md-6" style="line-height:35px;">
+                        {{ $product->price }}
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <label for="description" class="col-md-4 col-form-label text-md-end text-start"><strong>Description:</strong></label>
+                    <div class="col-md-6" style="line-height:35px;">
+                        {{ $product->description }}
                     </div>
                 </div>
 
                 <div class="row">
-                    <label for="name" class="col-md-4 col-form-label text-md-end text-start"><strong>Name:</strong></label>
+                    <label class="col-md-4 col-form-label text-md-end"><strong>Image:</strong></label>
                     <div class="col-md-6" style="line-height: 35px;">
-<<<<<<< HEAD
                         @if($product->hasImage())
                             <img src="{{ $product->image_url }}" alt="{{ $product->name }}" style="max-width:200px; height:auto;" class="img-thumbnail">
                         @else
                             <span class="text-muted">No Image</span>
                         @endif
-=======
-                        {{ $product->name }}
->>>>>>> 5834926ac5a32b91cfd1b3662a15453859262678
                     </div>
                 </div>
-
-                <div class="row">
-                    <label for="quantity" class="col-md-4 col-form-label text-md-end text-start"><strong>Quantity:</strong></label>
-                    <div class="col-md-6" style="line-height: 35px;">
-                        {{ $product->quantity }}
-                    </div>
-                </div>
-
-                <div class="row">
-                    <label for="price" class="col-md-4 col-form-label text-md-end text-start"><strong>Price:</strong></label>
-                    <div class="col-md-6" style="line-height: 35px;">
-                        {{ $product->price }}
-                    </div>
-                </div>
-
-                <div class="row">
-                    <label for="description" class="col-md-4 col-form-label text-md-end text-start"><strong>Description:</strong></label>
-                    <div class="col-md-6" style="line-height: 35px;">
-                        {{ $product->description }}
-                    </div>
-                </div>
-
-                <div class="row mt-3">
-    <label for="image" class="col-md-4 col-form-label text-md-end text-start"><strong>Image:</strong></label>
-    <div class="col-md-6">
-        @if($product->image_path)
-            <img src="{{ asset('storage/' . $product->image_path) }}" class="img-thumbnail" style="max-height: 200px;">
-        @else
-            <p>No image available.</p>
-        @endif
-    </div>
-</div>
-
-<div class="row">
-    <label for="image_path" class="col-md-4 col-form-label text-md-end text-start"><strong>Image Path:</strong></label>
-    <div class="col-md-6" style="line-height: 35px;">
-        {{ $product->image_path ?? 'No path saved.' }}
-    </div>
-</div>
-
-
             </div>
         </div>
-<<<<<<< HEAD
     </div> 
 </div> 
 @endsection
-=======
-    </div>
-</div>
-@endsection
->>>>>>> 5834926ac5a32b91cfd1b3662a15453859262678
